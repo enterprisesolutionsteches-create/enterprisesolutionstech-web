096dd9993dc053fa6cdc83e7213fad518d191554
Josue Diaz <josueac20@gmail.com>
2025-08-04 08:39:05 -0400

    fix issue

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,8 +1,7 @@
 import { FC } from "react";
-import { PageNotFound } from "../app/screens/PageNotFound";
 
 const NotPage: FC = () => {
-  return <PageNotFound />;
+  return "Pagina no encontrada";
 };
 
 export default NotPage;
